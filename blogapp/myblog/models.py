c006209cdbb644e85b626ebbe871407301e393c7
Atul18341 <atulkumar987613@gmail.com>
2021-03-20 14:53:19 +0530

    Template Updated

--- blogapp/myblog/models.py
+++ blogapp/myblog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class blog(models.Model):
-   post=models.TextField()
+   content=models.TextField()
    image=models.ImageField(upload_to='uploads')
 
    def __str__(self):
