149c1d9952da079d1764c11bd91f53e03930f299
Atul18341 <atulkumar987613@gmail.com>
2021-04-01 12:16:11 +0530

    Extended User Model

--- blogapp/myblog/models.py
+++ blogapp/myblog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 # Create your models here.
 class blog(models.Model):
    content=models.TextField()
@@ -7,3 +9,11 @@ class blog(models.Model):
 
    def __str__(self):
       return self.content
+
+class Profile(models.Model):
+   user=models.OneToOneField(User, on_delete=models.CASCADE)
+   name=models.CharField(max_length=100)
+   age=models.IntegerField()
+   gender=models.CharField(max_length=100)
+
+
